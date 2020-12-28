9046dac11a549a81351b8571eee35d0ad26262d7
Darko <c.darko3@gmail.com>
2020-12-28 18:52:46 +0100

    added action button with animation

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,21 @@
 <template>
   <q-page class="flex flex-center">
+    <q-btn :loading="loading" color="secondary" icon="settings" style="min-width: 150px" label="Generate" text-color="dark" @click="loading = !loading">
+      <template v-slot:loading>
+        <q-spinner-gears class="on-left"/>
+        Processing...
+      </template>
+    </q-btn>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      loading: false
+    }
+  },
 }
 </script>
