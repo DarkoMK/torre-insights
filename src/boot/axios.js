ed27f8e16392aff39a8139077036765529afc6d8
Darko <c.darko3@gmail.com>
2020-12-29 16:26:31 +0100

    axios config & get method for the generated data

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.headers['Content-Type'] = 'application/json'
+axios.defaults.headers['Accept'] = 'application/json'
 Vue.prototype.$axios = axios
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <q-img contain src="~/assets/torre_logo_small.png" style="width: 67px"></q-img>
     <div class="text-h5 text-center">Get insights on demographics, technologies, salaries directly from Torre's diverse database</div>
     <div class="text-h6 text-center">Press on "Generate" to get the latest Torre insights</div>
-    <q-btn :loading="loading" color="secondary" icon="settings" label="Generate" style="min-width: 150px" text-color="dark" @click="loading = !loading">
+    <q-btn :loading="loading" color="secondary" icon="settings" label="Generate" style="min-width: 150px" text-color="dark" @click="generateData">
       <template v-slot:loading>
         <q-spinner-gears class="on-left"/>
         Processing...
@@ -20,5 +20,19 @@ export default {
       loading: false
     }
   },
+  methods: {
+    generateData() {
+      this.loading = true
+      this.$axios.get('insights')
+        .then(r => {
+          this.$store.commit("insights/updateInsightsState", r.data)
+          this.$router.push({name: 'insights'})
+        })
+        .catch(e => {
+          // error handling
+          this.loading = false
+        })
+    }
+  }
 }
 </script>
