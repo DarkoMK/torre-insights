e44a20e697fa3532315f440e07bb2095c0aa1030
Darko <c.darko3@gmail.com>
2020-12-30 00:43:50 +0100

    added filter to select by ORG because the DB is too big to process all data

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,32 @@
     <q-img contain src="~/assets/torre_logo_small.png" style="width: 67px"></q-img>
     <div class="text-h5 text-center">Get insights on demographics, technologies, salaries directly from Torre's diverse database</div>
     <div class="text-h6 text-center">Press on "Generate" to get the latest Torre insights</div>
-    <q-btn :loading="loading" color="secondary" icon="settings" label="Generate" style="min-width: 150px" text-color="dark" @click="generateData">
+    <q-select
+      v-model="org"
+      :color="$q.dark.mode ? 'white':'dark'"
+      :disable="loading"
+      :loading="loadingOrgResults"
+      :options="options"
+      clearable
+      input-debounce="1000"
+      label="Select Organization"
+      option-label="name"
+      outlined
+      use-input
+      @filter="filterFn"
+    >
+      <template v-slot:prepend>
+        <q-icon name="business" @click.stop/>
+      </template>
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">
+            No results
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-select>
+    <q-btn :disable="!org" :loading="loading" color="secondary" icon="settings" label="Generate" style="min-width: 150px" text-color="dark" @click="generateData">
       <template v-slot:loading>
         <q-spinner-gears class="on-left"/>
         Processing...
@@ -17,13 +42,16 @@ export default {
   name: 'PageIndex',
   data() {
     return {
-      loading: false
+      loading: false,
+      org: {name: 'Torre Labs'},
+      options: [{name: 'Torre Labs'}],
+      loadingOrgResults: false
     }
   },
   methods: {
     generateData() {
       this.loading = true
-      this.$axios.get('insights')
+      this.$axios.get('insights/' + this.org.name)
         .then(r => {
           this.$store.commit("insights/updateInsightsState", r.data)
           this.$router.push({name: 'insights'})
@@ -36,6 +64,31 @@ export default {
           })
           this.loading = false
         })
+    },
+    filterFn(val, update) {
+      if (val === '') {
+        update(() => {
+        })
+        return
+      }
+      update(() => {
+        this.loadingOrgResults = true
+        const needle = val.toLowerCase()
+        this.$axios.get('organizations/' + needle)
+          .then(r => {
+            this.options = r.data
+            this.loadingOrgResults = false
+          })
+          .catch(e => {
+            // error handling
+            this.$q.notify({
+              type: 'negative',
+              message: 'An error occurred. Please try again later.'
+            })
+            this.loadingOrgResults = false
+          })
+        // get from api
+      })
     }
   }
 }
