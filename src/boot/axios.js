901988609e7e87091629946ff3b7d0ae904789f8
Darko <c.darko3@gmail.com>
2020-12-31 11:17:30 +0100

    replaced default api point

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "http://40.76.136.229:8000/api";
 axios.defaults.headers['Content-Type'] = 'application/json'
 axios.defaults.headers['Accept'] = 'application/json'
 Vue.prototype.$axios = axios
